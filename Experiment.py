c244e0136d703b0b28bd7868bdd3cd0b9e8fbe8f
Binbin FU <fubinbinole@gmail.com>
2014-11-03 18:14:43 -0600

    change code style of Experiment.py

--- Experiment.py
+++ Experiment.py
@@ -1,11 +1,16 @@
+"""
+Experiment
+"""
 import sys
-
-def largest(list):
-  if len(list) == 0:
-    raise ValueError("Cannot call largest on empty list")
-  max = -sys.maxint # "smallest" possible int
-  # max = 0
-  for index in range(len(list)):
-    if (list[index] > max):
-      max = list[index]
-  return max
+def largest(my_list):
+    """
+    largest
+    """
+    if len(my_list) == 0:
+        raise ValueError("Cannot call largest on empty list")
+    my_max = -sys.maxint # "smallest" possible int
+    # max = 0
+    for index in range(len(my_list)):
+        if (my_list[index] > my_max):
+            my_max = my_list[index]
+    return my_max
