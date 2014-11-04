0b5a345ae910456b1d9d32d07c4b82828e57a03c
Binbin FU <fubinbinole@gmail.com>
2014-11-03 18:44:55 -0600

    test result

--- nose.sh
+++ nose.sh
@@ -1,2 +1,3 @@
-nosetests -v --with-coverage ExperimentTest.py
-pylint -r n Experiment.py
+nosetests --with-xunit --all-modules --traverse-namespace --with-coverage ExperimentTest.py --cover-inclusive >& result.text
+python -m coverage xml --include=Experiment.py,ExperimentTest.py
+pylint -f parseable -d I0011,R0801 Experiment.py | tee pylint.out
